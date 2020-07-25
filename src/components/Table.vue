0e34045d8a82134d83461aeefe521c591254f248
DenisSoldugeev <den.soldugeev@yandex.ru>
2020-07-25 16:21:12 +0300

    add_sort

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,11 +3,12 @@
         <table class="table table-bordered table-striped">
             <thead class="thead-dark">
             <tr>
-                <th scope="col">id</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
+                <th scope="col" @click="sortById">id <i class="fas fa-sort-down" :class="{ 'fa-sort-up': sortId }"></i></th>
+                <th scope="col" @click="sortByFirstName">First Name <i class="fas fa-sort-down" :class="{ 'fa-sort-up': sortFirstName }"></i></th>
+                <th scope="col" @click="sortByLastName">Last Name <i class="fas fa-sort-down" :class="{ 'fa-sort-up': sortLastName }"></i></th>
                 <th scope="col">Email</th>
-                <th scope="col">Phone</th>
+                <th scope="col">Phone
+                </th>
             </tr>
             </thead>
             <tbody>
@@ -36,7 +37,6 @@
 </template>
 
 <script>
-
 import TableRow from "./TableRow";
     export default {
         name: "Table",
@@ -53,13 +53,16 @@ import TableRow from "./TableRow";
         },
         data: () => {
             return {
-                showUsers: 20,
-                pageNumber: 1
+                showUsers: 5,
+                pageNumber: 1,
+                sortFirstName: true,
+                sortLastName: true,
+                sortId: true
             }
         },
         computed: {
             pages() {
-                return  Math.ceil(this.users_data.length / this.showUsers)
+                return Math.ceil(this.users_data.length / this.showUsers)
             },
             paginationUsers() {
                 let from = (this.pageNumber - 1) * this.showUsers;
@@ -70,10 +73,35 @@ import TableRow from "./TableRow";
         methods: {
             pageOn(page) {
                 this.pageNumber = page;
-
+            },
+            sortByFirstName() {
+                if (this.sortFirstName) {
+                    this.users_data.sort((a, b) => a.firstName.localeCompare(b.firstName))
+                    this.sortFirstName = false
+                } else {
+                    this.users_data.sort((a, b) => b.firstName.localeCompare(a.firstName))
+                    this.sortFirstName = true
+                }
+            },
+            sortByLastName() {
+                if (this.sortLastName) {
+                    this.users_data.sort((a, b) => a.lastName.localeCompare(b.lastName))
+                    this.sortLastName = false
+                } else {
+                    this.users_data.sort((a, b) => b.firstName.localeCompare(a.lastName))
+                    this.sortLastName = true
+                }
+            },
+            sortById() {
+                if (this.sortId) {
+                    this.users_data.sort((a, b) => a.id - b.id)
+                    this.sortId = false
+                } else {
+                    this.users_data.sort((a, b) => b.id - a.id)
+                    this.sortId = true
+                }
             }
         }
-
     }
 </script>
 
@@ -81,8 +109,10 @@ import TableRow from "./TableRow";
  .table {
      width: 1140px;
  }
+ .table thead tr th {
+     cursor: pointer;
+ }
  .pagination {
      justify-content: center;
  }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import Paginate from 'vuejs-paginate';
 
 Vue.component('Paginate', Paginate)
